cb296192337d1b118652dd7a2890e19a3eee05fb
juandiegomoscoso <programming.uxh5y@simplelogin.co>
2024-12-16 22:32:17 -0300

    Agregar funcion registrar_producto
    Este funcion permite ingredar un producto y almacenarlo en la base de datos

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ def crear_base_datos():
     
     conexion.commit()
     conexion.close()
+
+
+def registrar_producto():
+    conexion = sqlite3.connect("inventario.db")
+    cursor = conexion.cursor()
+
+    nombre = input("Ingrese el nombre del producto: ")
+    cantidad = int(input("Ingrese la cantidad: "))
+    precio = float(input("Ingrese el precio: "))
+    categoria = input("Ingrese la categoria: ")
+
+    cursor.execute('''INSERT INTO Producto (nombre, cantidad, precio, categoria)
+                   VALUES (?, ?, ?, ?)''', (nombre, cantidad, precio, categoria))
+
+    conexion.commit()
+    conexion.close()
